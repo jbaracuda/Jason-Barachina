91c40a201d425a9b615282bd86b9c30494de6029
jbaracuda <jasonbarachina8@gmail.com>
2025-09-25 17:29:29 -0400

    Add files via upload

---
+++ WebDevLab01/WebDevLab01/Home_Page.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+# Title of App
+st.title("Welcome to my first ever Home Page!")
+#NEW
+st.balloons()
+st.subheader("Feel free to navigate anywhere you would like! :)")
+st.write('---')
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Web Development - Section B")
+st.subheader("Jason Barachina")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Page Name**: Description
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to our Streamlit Web Development Lab01 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1.Buzzfeed Quiz - Take the quiz and find out something new about yourself!
+
+2.Portfolio - Data about myself and things I have accomplished.
+""")
+
