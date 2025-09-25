f8467cfe736628e7be7a33d4452c154592b9c2aa
jbaracuda <jasonbarachina8@gmail.com>
2025-09-25 17:42:50 -0400

    Add files via upload

---
+++ WebDevLab01/WebDevLab01/info.py
@@ -0,0 +1,73 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/labPFP.jpeg"
+about_me = "Howdy! My name is Jason Barachina and I am a first-year Computer Science Major at Georgia Tech. I am a FANTASTIC programmer no matter what anyone says. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/jason-barachina-383046336/"
+my_github_url = "https://github.com/jbaracuda"
+my_email_address = "jbarachina3@gatech.edu"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2029(Hopefully)',
+    'GPA': '4.0(As of now)'
+}
+course_data = {
+    "code":["CS 1301", "CS 1100", "MATH 1551", "ENGL 1102","JAPN 1001", "APPH 1040"], 
+    "names":["Intro to CS", "CS Freshman Leap Seminar", "Differential Calculus", "English Literature", "Elementary Japanese","Sci Foundation of Health"], 
+    "semester_taken":["1st","1st","1st","1st","1st","1st"],
+    "skills":["Python is not my favorite snake anymore", "One helluva career fair expert!", "Repeated class from high school", "Had to do community service to pass...","こんにちは!!", "Learned all the ways NOT to develop a society"],
+    }
+experience_data = {
+    "Project Manager": (["- Organized and led sparring tournaments and public demonstrations",
+                                                                          "- Impressed many crowds and generated 34% bonus revenue."],"Images/tkdLABpic.JPG"),
+    "Public Performer":(["- Performed traditional martial arts material infront of large crowds"],"Images/auraLAB.JPG")
+}
+
+projects_data = {
+    "Steganography": "Worked with data encryption and decryption, as well as data compression.",
+}
+
+programming_data = {
+    "Python": 55,
+    "Java": 40,
+    "C": 0,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"Japanese": "🇯🇵",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Japanese": "Getting there...",
+    "Spanish": "Fluent",
+}
+leadership_data = {
+    "Martial Arts Instructor": (["- 3rd Degree Black Belt Jyokyonim Instuctor"],"Images/instructor.jpg"),
+
+}
+activity_data={
+    "Deerwood Academy STEM Education": (["- Taught children destructive and constructive natural forces, I.E erosion", 
+            "- Also taught children the foundations of engineering: Problem solving and teamwork"], "Images/comService.jpg"),
+}
